1e0935dc532b4f9f05a620c9dce5749939cdf0fd
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro.local>
2022-05-02 20:17:23 +0100

    Homework commit

---
+++ precourse_recap.py
@@ -0,0 +1,14 @@
+note1 = "here goes"
+note2 = " fingers crossed!"
+message = note1 + note2
+
+
+print(message.upper())
+
+
+g31_students = 21
+g31_staff = 4
+cohortg31 = g31_students + g31_staff
+
+
+print(cohortg31)

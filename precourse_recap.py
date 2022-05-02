ed0529b33767d742c18258b63bb33a2c051f9bbe
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro.local>
2022-05-02 21:24:59 +0100

    added budget calculation

--- precourse_recap.py
+++ precourse_recap.py
@@ -2,7 +2,6 @@ note1 = "here goes"
 note2 = " fingers crossed!"
 message = note1 + note2
 
-
 print(message.upper())
 
 
@@ -10,5 +9,12 @@ g31_students = 21
 g31_staff = 4
 cohortg31 = g31_students + g31_staff
 
-
 print(cohortg31)
+
+budget = 10
+lunch_cost = 6
+change = budget - lunch_cost
+
+print(change)
+
+
